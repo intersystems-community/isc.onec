bb3169bf91f0c1be705d8e812a6e04f4d7c3ee17
Andrey Shcheglov <andrey.shcheglov@intersystems.com>
2014-07-16 21:53:14 +0400

    Command moved out of server definition. Now a stand-alone type

---
+++ isc.onec.adapter/Command.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace isc.onec.bridge {
+	/// <summary>
+	/// Command types sent externally to this server.
+	/// </summary>
+	public enum Command {
+		/// <summary>
+		/// Get an object's property.
+		/// </summary>
+		GET = 1,
+		/// <summary>
+		/// Set an object's property.
+		/// </summary>
+		SET = 2,
+		/// <summary>
+		/// Invoke an instance method on an object.
+		/// </summary>
+		INVOKE = 3,
+		/// <summary>
+		/// Connect a client to an URL.
+		/// </summary>
+		CONNECT = 4,
+		/// <summary>
+		/// Disconnect.
+		/// </summary>
+		DISCONNECT = 5,
+		/// <summary>
+		/// Remove an object from local cache as well as release the corresponding COM object.
+		/// </summary>
+		FREE = 6,
+		/// <summary>
+		/// Return the amount of objects allocated as well as object currently live (cached).
+		/// </summary>
+		COUNT = 7,
+	}
+
+	public static class CommandExtensions {
+		public static object GetPrimitiveType(this Command command) {
+			return Convert.ChangeType(command, command.GetTypeCode());
+		}
+	}
+}
--- isc.onec.adapter/Server.cs
+++ isc.onec.adapter/Server.cs
@@ -10,40 +10,6 @@ namespace isc.onec.bridge {
 	/// Server -> V8Service -> {V8Adapter, Repository, Client}.
 	/// </summary>
 	public sealed class Server {
-		/// <summary>
-		/// Command types sent externally to this server.
-		/// </summary>
-		public enum Command {
-			/// <summary>
-			/// Get an object's property.
-			/// </summary>
-			GET = 1,
-			/// <summary>
-			/// Set an object's property.
-			/// </summary>
-			SET = 2,
-			/// <summary>
-			/// Invoke an instance method on an object.
-			/// </summary>
-			INVOKE = 3,
-			/// <summary>
-			/// Connect a client to an URL.
-			/// </summary>
-			CONNECT = 4,
-			/// <summary>
-			/// Disconnect.
-			/// </summary>
-			DISCONNECT = 5,
-			/// <summary>
-			/// Remove an object from local cache as well as release the corresponding COM object.
-			/// </summary>
-			FREE = 6,
-			/// <summary>
-			/// Return the amount of objects allocated as well as object currently live (cached).
-			/// </summary>
-			COUNT = 7,
-		};
-
 		private V8Service service;
 
 		private static Logger logger = LogManager.GetCurrentClassLogger();
