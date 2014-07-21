464538887b6a00f51c3b49fd674f11e2ec3166f9
Andrey Shcheglov <andrey.shcheglov@intersystems.com>
2014-07-21 13:41:53 +0400

    ComVisible set to false

--- isc.general/Properties/AssemblyInfo.cs
+++ isc.general/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1a942194-a4c4-47da-a4a8-a9d4f015cacd")]
--- isc.onec.tcp.async/Properties/AssemblyInfo.cs
+++ isc.onec.tcp.async/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5b10c659-1786-43b4-a77f-151033b58bf9")]
