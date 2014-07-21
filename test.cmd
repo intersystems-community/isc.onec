f7fe6176b779df5cce7d5aea29e9d74f13d02252
Andrey Shcheglov <andrey.shcheglov@intersystems.com>
2014-07-21 15:56:41 +0400

    Remove: never used.

--- test.cmd
+++
@@ -1,8 +0,0 @@
-rem
-set PDIR=%CD%
-
-cd %GLOBALS_HOME%\bin
-echo zn "ONECTEST"  for i=1:1:10 { job ##class(isc.onec.test.ContextDynamicTest).main() } | cache -s..\mgr
-echo zn "ONECTEST"  d ##class(isc.onec.test.ContextDynamicTest).main() | cache -s..\mgr
-
-cd %PDIR%
